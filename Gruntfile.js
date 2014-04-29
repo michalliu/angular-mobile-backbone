a091797181096bd0326a30cae83e741046fc740e
michalliu <michalliu@foxmail.com>
2014-04-30 04:12:30 +0800

    include iscrol-lite

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt){
 				separator: ';\n'
 			},
 			lib:{
-				src:["js/lib/zepto.js","js/lib/angular.js","js/lib/angular-*.js"],
+				src:["js/lib/zepto.js","js/lib/iscroll-lite.js","js/lib/angular.js","js/lib/angular-*.js"],
 				dest: tempfolder+"/js/lib.js"
 			},
 			page:{
