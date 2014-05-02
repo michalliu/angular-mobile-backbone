9b10ff6c11ab02612838bb62d026ec78e9cdc851
michalliu <michalliu@foxmail.com>
2014-05-02 09:03:20 +0800

    update index page, supports pull to refresh

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -1,4 +1,4 @@
-/*globals angular*/
+/*globals angular,$*/
 (function indexControllerInit() { "use strict";
 
 	angular.module("appControllers").
@@ -7,13 +7,57 @@
 		$scope.navigate=function(path) {
 			page.navigate(path);
 		};
+
+		var refreshScrollThreshold=100; // trigger the refresh action only if the scroll bigger than this value
+		var refreshTips=["Pull to refresh","Release to refresh","Loading..."];
+
 		$scope.items=[];
+
+		$scope.refreshTip=refreshTips[0];
+
+		$scope.myScrollOptions={
+			"indexViewScroll":{
+				onScrollMove: function (){
+					if(this.y > refreshScrollThreshold) {
+						if ($scope.refreshTip !== refreshTips[1]){
+							$scope.refreshTip=refreshTips[1];
+							$scope.$apply();
+						}
+					}
+					this.lastY=this.y;
+				},
+				onTouchEnd: function() {
+					if (this.lastY > refreshScrollThreshold) {
+						$("#indexViewScroller").css("top","48px"); // let "pull to refresh" visible
+					}
+				},
+				onScrollEnd: function () {
+					if (this.lastY > refreshScrollThreshold) {
+						if ($scope.refreshTip !== refreshTips[2]){
+							$scope.refreshTip=refreshTips[2];
+							$scope.$apply();
+						}
+						loadNewItem().then(function () {
+							$("#indexViewScroller").animate({
+								top: 0
+							}, 200, "ease", function () {
+								if ($scope.refreshTip !== refreshTips[0]){
+									$scope.refreshTip=refreshTips[0];
+									$scope.$apply();
+								}
+							}); // let "pull to refresh" invisible
+						});
+					}
+					this.lastY=this.y; // this.y should be 0 here
+				}
+			}
+		};
 
 		$timeout(function () {
 			$scope.items=new Array(60);
 		}, 1000);
 
-		$scope.$watch("items", iScrollRefresh);
+		$scope.$watch("items", iScrollRefresh, true);
 
 		function iScrollRefresh(){
 			$timeout(function () {
@@ -21,6 +65,15 @@
 					$scope.myScroll.indexViewScroll.refresh();
 				}
 			},0);
+		}
+
+		var newItemId=0;
+		function loadNewItem() {
+			return $timeout(function () {
+				for (var i=0;i<10;i++){
+					$scope.items.unshift("new item " + newItemId++);
+				}
+			},2000);
 		}
 	}]);
 }());
