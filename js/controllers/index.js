693e8a6ebafc4aad8cc5f56eb7eff8111c25c0fa
michalliu <michalliu@foxmail.com>
2014-05-03 05:53:08 +0800

    simplify index page

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -15,56 +15,8 @@
 			$scope.items=new Array(60);
 		}, 1000);
 
-		var pullDownMessages=["Pull down to refresh","Release to refresh","Loading..."];
-		var scrollOffset=$("#indexViewPullDown").height();
-		var pullDownThreshold=30;
-
-		$scope.pullDownMessage=pullDownMessages[0];
-
-		$scope.myScrollOptions={
-			"indexViewScroll": {
-				topOffset:scrollOffset,
-				onScrollMove: function () {
-					if (this.y > pullDownThreshold) {
-						if ($scope.pullDownMessage !== pullDownMessages[1]) {
-							$scope.pullDownMessage=pullDownMessages[1];
-							$scope.$apply();
-						}
-						this.minScrollY=0;
-					}
-					this.oldY=this.y;
-				},
-				onScrollEnd: function () {
-					if (!this.scrollInit){
-						$("#indexViewPullDown").css("visibility","visible");
-						this.scrollInit=true;
-					}
-					if (this.oldY > pullDownThreshold) {
-						if ($scope.pullDownMessage !== pullDownMessages[2]) {
-							$scope.pullDownMessage=pullDownMessages[2];
-							$scope.$apply();
-						}
-						var that = this;
-						loadNewItem().then(function () {
-							that.minScrollY=-scrollOffset;
-							$scope.pullDownMessage = pullDownMessages[0];
-						});
-					}
-					this.oldY=this.y;
-				}
-			}
-		};
 
 		$scope.$watch("items", iScrollRefresh, true);
-
-		var newItemId=0;
-		function loadNewItem() {
-			return $timeout(function () {
-				for (var i=0;i<5;i++){
-					$scope.items.unshift("new item " + newItemId++);
-				}
-			}, 2000);
-		}
 
 		function iScrollRefresh(){
 			$timeout(function () {
