bca0cb063a237916f97e4bb6f5640eee6736245f
michalliu <michalliu@foxmail.com>
2014-12-01 17:52:26 +0800

    检查是否登录

--- js/app.js
+++ js/app.js
@@ -16,5 +16,11 @@
 				rootScope.$on("$stateChangeSuccess", function () {
 					ionicLoading.hide();
 				});
+		}]).run(["utilService","$window", function (util, win) {
+			var profile = util.parseJsonData("profile");
+			var sid = profile.sid;
+			if (!sid) {
+				win.location.href = "http://ui.ptlogin2.qzone.com/cgi-bin/login?style=9&appid=1000000000&pt_ttype=1&s_url=http%3A%2F%2Fttest.m.qzone.com%2Fwish%2Flogin";
+			}
 		}]);
 })();
