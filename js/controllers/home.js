868e0f3a6da331231b56ee13d41b252efed802af
michalliu <michalliu@foxmail.com>
2014-12-04 17:34:20 +0800

    不显示没有更多数据

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -148,7 +148,7 @@
 						if (wishList && wishList.length > 0) {
 							scope.items=concat(scope.items, processWishList(wishList, util));
 						} else {
-							page.dialog.toast("没有更多数据了");
+							//page.dialog.toast("没有更多数据了");
 						}
 						if (scope.items.length > 100 || res.data.hasmore === 0) { // 最多100条最新数据
 							scope.moredata = true; // 不再加载更多
