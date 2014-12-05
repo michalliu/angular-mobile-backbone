6159fa1b26dbc2f46d1b22a58c7fdec5d134b646
michalliu <michalliu@foxmail.com>
2014-12-05 16:54:06 +0800

    角标逻辑

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-/* globals angular */
+/* globals angular,$ */
 ;(function () {
 	angular
 		.module("app", [
@@ -37,6 +37,10 @@
 			var profile = util.parseJsonData("profile");
 			if (!profile || !profile.sid) {
 				win.location.href = "http://ui.ptlogin2.qzone.com/cgi-bin/login?style=9&appid=1000000000&pt_ttype=1&s_url=http%3A%2F%2Fttest.m.qzone.com%2Fwish%2Flogin";
+				return;
+			}
+			if (profile.new_msg_count > 0) {
+				$("#tab2badge").removeClass("x-init-hide").text(profile.new_msg_count);
 			}
 		}]);
 })();
